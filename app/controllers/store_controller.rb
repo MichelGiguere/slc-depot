d14231da9721f263b63aad23693979ec80d55825
Michel Giguere <michel.giguere@shopify.com>
2017-07-19 13:19:06 -0400

    add cart functionality

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
+  def increment_count
+  session[:counter] ||= 0
+  session[:counter] += 1
+  end
+  # session[:counter] += 1
+  #     if session[:counter] > 5
+  #       return session[:counter]
+  #     end
+  #   end
+
   def index
     @products = Product.order(:title)
+    @count = increment_count
+    #@count_message = "You've been here #{@count} times" if session [:counter] > 5
   end
 end
