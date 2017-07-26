852c75ca3a0c5f1141cc1f30e70716795344df03
betty <makewithbetty@gmail.com>
2017-07-26 15:50:58 -0400

    Tweak action cable config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://fierce-sea-91643.herokuapp.com/cable'
+  # config.web_socket_server_url = "wss://action-cable-example.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = [ 'https://fierce-sea-91643.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
